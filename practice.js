c0a6d42547c05614e4934983e62d028226afdfef
Richard Pitts <richardepitts@gmail.com>
2019-08-04 00:57:07 +0000

    Automatically backed up by Learn

--- practice.js
+++ practice.js
@@ -1,9 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+var bar = "ProteinBar"
+
 function addElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  return array.unshift(element);
 }
 
+console.log(addElementToBeginningOfArray(chocolateBars, bar));
 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
